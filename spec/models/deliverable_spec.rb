050df9fb3c29ea5e213ca4fa972cdc8065e5ecce
Drew Bomhof <drew.bomhof@limos.com>
2015-03-28 15:45:23 -0400

    Fixed the deliverable_spec to test the correct case of building the deliverable - happens on save of an Sms record

--- spec/models/deliverable_spec.rb
+++ spec/models/deliverable_spec.rb
@@ -1,13 +1,30 @@
 require 'factory_girl'
 
 describe Deliverable do
+  before(:all) do
+    Accounting.delete_all
+    Customer.delete_all
+    Sms.delete_all
+    Deliverable.delete_all
+  end
+
   context "Validations" do
-    it "validates to and to_deliver are unique" do
+    it "validates to and from phone numbers are unique" do
       FactoryGirl.create :customer
       FactoryGirl.create :accounting
 
-      sms = Sms.new(name: 'EEEYY', customer_id: Customer.last.id, body: "blah blah", to_phone: 4569875434, accounting: Accounting.last)
-      sms.save
+      sms = Sms.new(name: 'EEEYY', customer_id: Customer.last.id, to_deliver: 7653459889, body: "blah blah", to_phone: 4569875434, accounting: Accounting.last)
+      expect(sms.valid?).to eq true
+      expect(sms.save).to eq true
+    end
+
+    it "fails validation if the to and from phone numbers are unique" do
+      FactoryGirl.create :customer
+      FactoryGirl.create :accounting
+
+      sms = Sms.new(name: 'EEEYY', customer_id: Customer.last.id, to_deliver: 4569875434, body: "blah blah", to_phone: 4569875434, accounting: Accounting.last)
+      expect(sms.valid?).to eq true
+      expect(sms.save).to eq false
     end
   end
 end
